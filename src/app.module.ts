ecbc067598815c3a94f4c0cb8c7416d6a1f48461
MauroMontan <mauroanastacio@outlook.es>
2022-09-08 16:04:01 -0500

    database configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+//WARNING: replace with the right database info
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true, //TODO: change false on prod
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
