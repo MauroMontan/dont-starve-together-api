b13e841c0eec1441755160837c84bb326c266f79
MauroMontan <mauroanastacio@outlook.es>
2022-09-09 21:02:56 -0500

    get items endpoint created

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ItemsService } from './items.service';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private service: ItemsService) {}
+
+  @Get()
+  async ListAllItems() {
+    return await this.service.getAll();
+  }
+}
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Item } from './entities/entities';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item) private itemRepository: Repository<Item>,
+  ) {}
+
+  async getAll() {
+    return this.itemRepository.find();
+  }
+}
