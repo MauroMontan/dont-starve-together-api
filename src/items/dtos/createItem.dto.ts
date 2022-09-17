befb53251eb795088169a08ff3c23e89661f3769
MauroMontan <mauroanastacio@outlook.es>
2022-09-16 20:47:51 -0500

    class validator added

--- src/items/dtos/createItem.dto.ts
+++ src/items/dtos/createItem.dto.ts
@@ -1,16 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsString } from 'class-validator';
 import { ItemCategory } from '../enums/enums';
 
 export class CreateItemDto {
   @ApiProperty()
+  @IsString()
   name: string;
 
   @ApiProperty()
+  @IsString()
   asset: string;
 
   @ApiProperty()
+  @IsString()
   description: string;
 
   @ApiProperty()
+  @IsArray()
   category: ItemCategory[];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -15,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
