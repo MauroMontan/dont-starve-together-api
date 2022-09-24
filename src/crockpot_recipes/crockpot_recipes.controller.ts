4853d2382f4aa4b18563145ab2fbcb56e52d868f
MauroMontan <mauroanastacio@outlook.es>
2022-09-23 22:43:20 -0500

    C-recipe dto added to service,controller

--- src/crockpot_recipes/crockpot_recipes.controller.ts
+++ src/crockpot_recipes/crockpot_recipes.controller.ts
@@ -1,19 +1,23 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CrockpotRecipesService } from './crockpot_recipes.service';
+import { CreateCrockpotRecipeDto } from './dtos/dtos';
+import { CrockpotRecipe } from './entities/entities';
 
 @ApiTags('Crockpot Recipes')
 @Controller('crockpot-recipes')
 export class CrockpotRecipesController {
-  constructor(private service: CrockpotRecipesService) {}
+  constructor(private service: CrockpotRecipesService) { }
 
   @Post()
-  async createCrockpotRecipe(@Body() recipe: any) {
+  async createCrockpotRecipe(
+    @Body() recipe: CreateCrockpotRecipeDto,
+  ): Promise<CrockpotRecipe> {
     return this.service.create(recipe);
   }
 
   @Get()
-  async listRecipes() {
+  async listRecipes(): Promise<CrockpotRecipe[]> {
     return await this.service.getAll();
   }
 }
--- src/crockpot_recipes/crockpot_recipes.service.ts
+++ src/crockpot_recipes/crockpot_recipes.service.ts
@@ -2,19 +2,20 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CrockpotRecipe } from './entities/entities';
 import { Injectable } from '@nestjs/common';
+import { CreateCrockpotRecipeDto } from './dtos/dtos';
 
 @Injectable()
 export class CrockpotRecipesService {
   constructor(
     @InjectRepository(CrockpotRecipe)
     private CrockpotRecipeRepository: Repository<CrockpotRecipe>,
-  ) {}
+  ) { }
 
-  async create(recipe: CrockpotRecipe) {
+  async create(recipe: CreateCrockpotRecipeDto): Promise<CrockpotRecipe> {
     return this.CrockpotRecipeRepository.save(recipe);
   }
 
-  async getAll() {
+  async getAll(): Promise<CrockpotRecipe[]> {
     return await this.CrockpotRecipeRepository.find({
       relations: {
         stats: true,
