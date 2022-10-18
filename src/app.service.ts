b315eca46e4613da9d75b84d9353a1eee4ab1b8f
MauroMontan <mauroanastacio@outlook.es>
2022-10-18 14:43:32 -0500

    adding info

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      author: 'MauroMontan',
+      About: 'Yeah, im working on a client app :p, go to "/docs" to see more. ',
+    };
   }
 }
