01bf9f2827cf8869b91982573ac5e77301e5911e
MauroMontan <mauroanastacio@outlook.es>
2023-05-09 19:24:46 -0600

    added Roughage to food types

--- src/crockpot_recipes/enums/foodType.enum.ts
+++ src/crockpot_recipes/enums/foodType.enum.ts
@@ -1,7 +1,8 @@
 export enum FoodType {
-  MEAT = 'Meat',
-  VEGGIE = 'Veggie',
-  GOODIES = 'Goodies',
-  EDIBLE = 'Edible',
-  ELEMENTAL = 'Elemental',
+	MEAT = 'Meat',
+	VEGGIE = 'Veggie',
+	GOODIES = 'Goodies',
+	EDIBLE = 'Edible',
+	ELEMENTAL = 'Elemental',
+	ROUGHAGE = "Roughage"
 }
