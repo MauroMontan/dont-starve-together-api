12bda84e6c4160afcd4f47d701a74390ca86b692
MauroMontan <mauroanastacio@outlook.es>
2023-05-10 15:35:17 -0600

    redirecting client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
+  @Redirect("https://dstclient-1-w0962473.deta.app")
   @Get()
   getHello() {
     return this.appService.getHello();
